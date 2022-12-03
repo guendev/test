5109f60ec2ae8a3831115a33842762bf977d728a
yuan <nguyenshort@gmail.com>
2022-12-03 08:40:53 +0700

    cm

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <suspense>
 <!--    <master-layout />-->
     <div class="h-screen w-full flex justify-center items-center">
-      <div class="text-[30px]">Hello Thầy Ý</div>
+      <div class="text-[30px]">Hello Thầy Ý Đẹp Zai</div>
     </div>
   </suspense>
 </template>
